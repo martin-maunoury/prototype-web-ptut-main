bbf38cbfbddf88498b6e9e9b904f50d067ae75d5
martin-maunoury <martinmaumau@outlook.fr>
2021-02-10 18:29:18 +0100

    Modification header
    suppression des plugin WooCommerce
    suppression doublons dans le header de la page initiale

--- app/public/wp-content/plugins/theme-junkie-custom-css/assets/js/tjcc.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($){
-	var editor = CodeMirror.fromTextArea(document.getElementById("tjcc-custom-css-textarea"), {
-		mode: "css",
-		theme: "default",
-		styleActiveLine: true,
-		matchBrackets: true,
-		lineNumbers: true
-	});
-})
